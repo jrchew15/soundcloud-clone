304e13ea6b19f05199e83f1215606db0a96453ff
Jason Chew <jrchew15@gmail.com>
2022-07-29 13:57:31 -0700

    added image model/migration, needs references in migration

---
+++ db/migrations/20220729205003-create-image.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Images', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      imageableId: {
+        type: Sequelize.INTEGER,
+        // references:{
+        // TODO: find out how to reference multiple tables
+        // }
+      },
+      imageableType: {
+        type: Sequelize.ENUM('song', 'album', 'artist', 'playlist')
+      },
+      url: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Images');
+  }
+};
