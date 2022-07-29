9f1011dc8a674d395e931b45d1ffaa86f06f13e2
Jason Chew <jrchew15@gmail.com>
2022-07-29 13:49:06 -0700

    added songs model/migration

---
+++ db/migrations/20220729203620-create-song.js
@@ -0,0 +1,51 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Songs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id'
+        }
+      },
+      albumId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Albums',
+          key: 'id'
+        }
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      url: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Songs');
+  }
+};
---
+++ db/models/song.js
@@ -0,0 +1,58 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Song extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Song.belongsTo(models.Album, {
+        foreignKey: 'albumId'
+      });
+      Song.belongsTo(models.User, {
+        foreignKey: 'userId'
+      });
+      Song.hasMany(models.Image, {
+        foreignKey: 'imageableId'
+      });
+      Song.belongsToMany(models.Playlist, {
+        through: 'PlaylistSong',
+        otherKey: 'playlistId',
+        foreignKey: 'songId'
+      });
+    }
+  }
+  Song.init({
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    albumId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isUrl: true
+      }
+    },
+  }, {
+    sequelize,
+    modelName: 'Song',
+  });
+  return Song;
+};
