376597f4f7ce4f9cb861b210485d7d181f9900c1
Jason Chew <jrchew15@gmail.com>
2022-07-29 16:27:30 -0700

    log in/out/restore endpoints

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 const { restoreUser } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
 router.use(restoreUser);
 
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
 
 module.exports = router;
