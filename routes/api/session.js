eb84705984440e32072be977fee8216a9c65af2a
Jason Chew <jrchew15@gmail.com>
2022-08-01 13:41:00 -0700

    login endpoint

--- routes/api/session.js
+++ routes/api/session.js
@@ -10,10 +10,10 @@ const validateLogin = [
     check('credential')
         .exists({ checkFalsy: true })
         .notEmpty()
-        .withMessage('Please provide a valid email or username.'),
+        .withMessage('Email or username is required'),
     check('password')
         .exists({ checkFalsy: true })
-        .withMessage('Please provide a password.'),
+        .withMessage('Password is required'),
     handleValidationErrors
 ];
 
@@ -27,17 +27,17 @@ router.post(
         const user = await User.login({ credential, password });
 
         if (!user) {
-            const err = new Error('Login failed');
+            const err = new Error('Invalid credentials');
             err.status = 401;
-            err.title = 'Login failed';
-            err.errors = ['The provided credentials were invalid.'];
             return next(err);
         }
 
-        await setTokenCookie(res, user);
+        const token = await setTokenCookie(res, user);
+
+        const { id, firstName, lastName, email, username } = user;
 
         return res.json({
-            user
+            id, firstName, lastName, email, username, token
         });
     }
 );
