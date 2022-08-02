3036cf40b577fccd1a59fa9c4e69db3aa3a68185
Jason Chew <jrchew15@gmail.com>
2022-08-02 13:26:42 -0700

    album details by id endpoint

--- routes/api/albums.js
+++ routes/api/albums.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const { User, Album, Song } = require('../../db/models');
-const { albumFormatter } = require('../../utils/sanitizers.js');
+const { albumFormatter, songFormatter } = require('../../utils/sanitizers.js');
 const { requireAuth } = require('../../utils/auth.js');
+const { checkAlbumExists } = require('../../utils/db-checks.js')
 
 router.get('/current', requireAuth, async (req, res, next) => {
     let Albums = await Album.findAll({
@@ -10,6 +11,22 @@ router.get('/current', requireAuth, async (req, res, next) => {
     Albums = Albums.map(albumFormatter);
 
     res.json({ Albums });
+})
+
+router.get('/:albumId', async (req, res, next) => {
+    const album = await Album.findByPk(req.params.albumId, {
+        include: [
+            { model: User, attributes: ['id', 'username', ['imageUrl', 'previewImage']] },
+            { model: Song }
+        ]
+    });
+    if (!album) {
+        const err = new Error("Album couldn't be found");
+        err.status = 404;
+        return next(err);
+    }
+
+    res.json(albumFormatter(album));
 })
 
 router.get('/', async (_req, res, _next) => {
--- utils/sanitizers.js
+++ utils/sanitizers.js
@@ -1,8 +1,10 @@
 const { User, Album, Song } = require('../db/models');
 
 function songFormatter(song) {
-    if (!(song instanceof Song)) { throw new Error('Expected a song') }
-    song = song.toJSON();
+    if (song instanceof Song) {
+        song = song.toJSON();
+    }
+
     const result = {};
     for (let key of ['id', 'userId', 'albumId', 'title', 'description', 'url', 'createdAt', 'updatedAt']) {
         result[key] = song[key];
@@ -15,14 +17,18 @@ function songFormatter(song) {
 }
 
 function albumFormatter(album) {
-    if (!(album instanceof Album)) { throw new Error('Expected a album') }
-    album = album.toJSON();
+    if (album instanceof Album) {
+        album = album.toJSON();
+    }
+
     const result = {};
     for (let key of ['id', 'userId', 'title', 'description', 'createdAt', 'updatedAt']) {
         result[key] = album[key];
     }
     result.previewImage = album.imageUrl;
     result.Artist = album.User;
+    console.log(album.Songs[0])
+    result.Songs = album.Songs ? album.Songs.map(songFormatter) : undefined;
 
     return result;
 }
