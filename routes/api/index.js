e9b6b6910d5480f0cd5daa0e76425b4f84914107
Jason Chew <jrchew15@gmail.com>
2022-08-02 11:16:59 -0700

    album router setup

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,13 +2,16 @@ const router = require('express').Router();
 const { restoreUser } = require('../../utils/auth.js');
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
-const songsRouter = require('./songs.js')
+const albumsRouter = require('./albums.js');
+const songsRouter = require('./songs.js');
 
 router.use(restoreUser);
 
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
+
+router.use('/albums', albumsRouter);
 
 router.use('/songs', songsRouter);
 
