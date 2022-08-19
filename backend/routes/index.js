3e416899adeffa0444fb66bd1c7b202dd106b84b
Jason Chew <jrchew15@gmail.com>
2022-08-19 16:38:04 -0700

    fixed import bug

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,4 +1,5 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 const apiRouter = require('./api');
 
 // router.get("/api/csrf/restore", (req, res) => {
