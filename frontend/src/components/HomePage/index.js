5d75cf79070b8c9170179ab9d707d89146b5490a
Jason Chew <jrchew15@gmail.com>
2022-08-28 16:43:03 -0700

    two random songs

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -14,7 +14,8 @@ export default function HomePage() {
     const dispatch = useDispatch();
     const history = useHistory();
     const [headerBanner, setHeaderBanner] = useState(true);
-    const [randomSong, setRandomSong] = useState(null);
+    const [randomSong1, setRandomSong1] = useState(null);
+    const [randomSong2, setRandomSong2] = useState(null);
 
     useEffect(() => {
         const headerTimeout = setTimeout(() => {
@@ -24,22 +25,35 @@ export default function HomePage() {
     }, [headerBanner])
 
     useEffect(() => {
-        getRandomSong();
-        async function getRandomSong() {
-            const num = Math.ceil(Math.random() * 145);
+        getRandomSongs();
+        async function getRandomSongs() {
+            const num1 = Math.ceil(Math.random() * 145);
+            const num2 = Math.ceil(Math.random() * 145);
 
-            const res = await csrfFetch(`/api/songs/${num}`);
-            const song = await res.json();
-            setRandomSong(song);
+            const res1 = await csrfFetch(`/api/songs/${num1}`);
+            const song1 = await res1.json();
+            setRandomSong1(song1);
+
+            const res2 = await csrfFetch(`/api/songs/${num2}`);
+            const song2 = await res2.json();
+            setRandomSong2(song2);
         }
     }, [])
 
-    async function getNewRandomSong() {
+    async function getNewRandomSong1() {
         const num = Math.ceil(Math.random() * 145);
 
         const res = await csrfFetch(`/api/songs/${num}`);
         const song = await res.json();
-        setRandomSong(song);
+        setRandomSong1(song);
+    }
+
+    async function getNewRandomSong2() {
+        const num = Math.ceil(Math.random() * 145);
+
+        const res = await csrfFetch(`/api/songs/${num}`);
+        const song = await res.json();
+        setRandomSong2(song);
     }
 
     const rightButtons = (
@@ -99,20 +113,42 @@ export default function HomePage() {
                 </div>
             </div >
             <div id='homepage-content'>
-                {randomSong && (<div>
+                {randomSong1 && randomSong2 && (<div style={{ width: '95%' }}>
                     <h2>Try the Song Queue:</h2>
                     <div id='queue-test'>
-                        <img src={randomSong.previewImage || default_album_image} style={{ gridRow: '1/3', gridColumn: '1', width: '100%' }} onError={(e) => e.target.src = default_album_image} />
-                        <button onClick={() => dispatch(playThis(randomSong))}>Play song now</button>
-                        <button onClick={() => dispatch(actionConcatToQueue([randomSong]))}>Add to queue</button>
-                        <button onClick={getNewRandomSong}>get a different song</button>
-                        <span id='queue-test-song-info' style={{ gridRow: 3 }}>
-                            <span onClick={() => history.push(`/songs/${randomSong.id}`)}>{randomSong.title}</span>
-                            {' by '}
-                            <span onClick={() => history.push(`/users/${randomSong.Artist.id}`)}>{randomSong.Artist.username}</span>
-                        </span>
+                        <div style={{ display: 'flex' }}>
+                            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                <img src={randomSong1.previewImage || default_album_image} style={{ width: '240px' }} onError={(e) => e.target.src = default_album_image} />
+                                <div className='queue-test-song-info' >
+                                    <span onClick={() => history.push(`/songs/${randomSong1.id}`)}>{randomSong1.title}</span>
+                                    {' by '}
+                                    <span onClick={() => history.push(`/users/${randomSong1.Artist.id}`)}>{randomSong1.Artist.username}</span>
+                                </div>
+                            </div>
+                            <div className='queue-test-buttons'>
+                                <button onClick={() => dispatch(playThis(randomSong1))}>Play song now</button>
+                                <button onClick={() => dispatch(actionConcatToQueue([randomSong1]))}>Add to queue</button>
+                                <button onClick={getNewRandomSong1}>Get a different song</button>
+                            </div>
+                        </div>
+                        <div style={{ display: 'flex' }}>
+                            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                <img src={randomSong2.previewImage || default_album_image} style={{ width: '240px' }} onError={(e) => e.target.src = default_album_image} />
+                                <div className='queue-test-song-info' >
+                                    <span onClick={() => history.push(`/songs/${randomSong2.id}`)}>{randomSong2.title}</span>
+                                    {' by '}
+                                    <span onClick={() => history.push(`/users/${randomSong2.Artist.id}`)}>{randomSong2.Artist.username}</span>
+                                </div>
+                            </div>
+                            <div className='queue-test-buttons'>
+                                <button onClick={() => dispatch(playThis(randomSong2))}>Play song now</button>
+                                <button onClick={() => dispatch(actionConcatToQueue([randomSong2]))}>Add to queue</button>
+                                <button onClick={getNewRandomSong2}>Get a different song</button>
+                            </div>
+                        </div>
                     </div>
                 </div>)}
+                <Separator />
                 <div id='homepage-sampler'>
                     <h2>A Sampling of Detail Pages:</h2>
                     <ul>
@@ -125,4 +161,8 @@ export default function HomePage() {
             </div>
         </>
     )
+
+    function Separator() {
+        return <div style={{ minHeight: '2px', width: '90%', backgroundColor: '#ebebeb' }}></div>
+    }
 }
